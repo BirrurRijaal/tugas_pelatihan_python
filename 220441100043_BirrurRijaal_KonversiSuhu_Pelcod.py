a0058ec223c74e575b62711c7ba26d86e7b2ec73
BirrurRijaal <113223425+BirrurRijaal@users.noreply.github.com>
2022-09-16 16:32:56 +0700

    Add files via upload

---
+++ 220441100043_BirrurRijaal_KonversiSuhu_Pelcod.py
@@ -0,0 +1,18 @@
+# Mengkonversi suhu Celcius ke Fahrenheit
+print("\nKonversi Terperatur\n")
+
+#Celcius
+Celcius = float(input("Masukkan besar suhu Celcius :" ))
+print()
+
+#Reamur
+Reamur = ((4/5) * Celcius)
+print("Hasil konversi ke Reamur adalah :",Reamur,"˚Reamur")
+
+#Fahrenheit
+Fahrenheit = ((9/5) * Celcius)+32
+print("Hasil konversi ke Fahrenheit adalah :",Fahrenheit,"˚Fahrenheit")
+
+#Kelvin
+Kelvin = Celcius + 273
+print("Hasil konversi ke Kelvin adalah :",Kelvin,"Kelvin")
